96f749e10f2cfbc44def1e91ec58559ff7743605
aob_wedev <20451158+aobcoopy@users.noreply.github.com>
2023-08-07 21:04:23 +0700

    update css 07-08-66

--- libs/pages/00-new_style.php
+++ libs/pages/00-new_style.php
@@ -1,9 +1,21 @@
 <style>
-.co_orange, ul.mg-footer-social > li > a
+/*blue
+183250
+
+orange
+e79424
+
+gray
+808184*/
+.buttag
+{
+    background:rgb(232 150 35 / 80%);
+}
+.co_orange, ul.mg-footer-social > li > a,.btn-main,#bt_search_1,.btn-dark-main2,.top_en_box,.bt_top_enbox,.baa,.but_ques,.acct,.exchange,.bggold,.rev_pop,.btn_enq,.qbut,.btn_villa_2 
 {
 	background:#e79424 !important;
 }
-.co_orange:hover,.mg-footer-social > li > a:hover
+.co_orange:hover,.mg-footer-social > li > a:hover,.btn_subscribe:hover,.btn-main:hover,#bt_search_1:hover,.btn-dark-main2:hover,.but_ques:hover,.baa:hover,.rev_pop:hover,.btn_enq:hover,.btn_villa_2:hover
 {
 	background:#FFBB60 !important;
 }
@@ -23,7 +35,7 @@ ul.menulinkfooter2 > li > a:hover
 {
 	background:#808184 !important;
 }
-.txt_co_orange
+.txt_co_orange,.ffix
 {
 	color:#e79424 !important;
 }
@@ -37,4 +49,77 @@ ul.menulinkfooter2 > li > a:hover
 .mg-feature:hover .mg-feature-icon-title i {
   background-color: #e79424;
 }
+
+.boxbot:hover {
+    background-color: #112845;
+    color: #fff;
+    border-bottom: 2px solid #e79424;
+}
+.dot {
+    width: 12px;
+    height: 12px;
+    background: #e79424;
+    border-radius: 100%;
+    margin-left: -37px;
+    margin-top: 0px;
+    position: absolute;
+}
+.ft	
+{
+	background: #e79424 !important;
+}
+.bline {
+    border-left: 2px solid #e79424;
+    padding: 0px 0px 20px 0px;
+}	
+.btn_subscribe
+{
+	background: #e79424 !important;
+}
+.nav > li.active > a,.nav > li:hover > a
+{
+ color:#e89623 !important;
+}
+.nav > li.dropdown ul.dropdown-menu li:hover a
+{
+  background:#e89623 !important;
+  color:#112845 !important;
+}	
+ol.breadcrumb li:hover a,ol.breadcrumb li.active a
+{
+ color:#e89623 !important;
+}	
+.tgold 
+{
+color:#e89623 !important;
+}
+p > a > span 
+{
+ color:#E89623 !important;
+}
+.tags_bar
+{
+ background:#e89623 !important;
+}
+
+.btn_enquire,.bad  {
+    background-color: #e89623;
+}
+.read_more_box  a ,.read_more_box  .butrec > span
+{
+ color:#e79424 !important;
+}
+.bggray button
+{
+ color:#e79424 !important;
+}
+
+
+
+
+
+	
+	
+	
 </style>
+
